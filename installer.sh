1e137518f294e013e892c8c4f4fa720b9015d490
Vanished <155773111+v4nished@users.noreply.github.com>
2024-04-09 23:18:06 -0400

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,20 @@
+# Very much a wip
+
+#debian/ubuntu
+echo Welcome to YuZu Helper 1.0
+echo Installing Git Please wait
+sudo apt install git
+echo Git Installed
+git clone https://gitlab.com/v4nishedlol/yuzuwm.git
+echo Installing TaskSel
+sudo apt install tasksel
+echo PLEASE SELECT KDE/PLASMA WHEN TASKSEL OPENS
+echo 1
+echo 2
+echo 3
+sudo tasksel
+echo KDE Installed
+echo Rebooting Please Wait
+echo not rebooting lol
+
+
